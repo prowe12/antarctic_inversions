07fc16931b1ca022f36b8e43a1666bdf3fc67238
Penny Rowe <1795738+prowe12@users.noreply.github.com>
2023-02-11 14:38:18 -0800

    Improve type hints, improve adherence to convention, and add docstrings

--- plot_results.py
+++ plot_results.py
@@ -8,10 +8,28 @@ Created on Thu Feb  9 09:49:29 2023
 
 import matplotlib.pyplot as plt  # type: ignore
 import numpy as np
+import numpy.typing as npt
 from netCDF4 import Dataset  # type: ignore
 
 
-def plot_final_results(lat, lon, frequency, depth, strength, tsurf):
+def plot_final_results(
+    lat: npt.NDArray[np.float64],
+    lon: npt.NDArray[np.float64],
+    frequency: npt.NDArray[np.float64],
+    depth: npt.NDArray[np.float64],
+    strength: npt.NDArray[np.float64],
+    tsurf: npt.NDArray[np.float64],
+):
+    """
+    Plot results for averages over multiple years
+    @params lat  The latitude
+    @params lon  The longitude
+    @param frequency  The mean frequency of occurence of inversions (%)
+    @params depth  The mean inversion depth
+    @params strength  = The mean inversion intensity
+    @params tsurf  = The mean surface temperature
+    Notes: freq, depth, strength, and tsurf are all functions of lat and lon
+    """
 
     # plt.figure(3)
     # plt.clf()
@@ -25,11 +43,11 @@ def plot_final_results(lat, lon, frequency, depth, strength, tsurf):
     # plt.plot(lat, strength, ".-")
     # plt.ylabel("Mean inversion strength (C)")
 
-    x, y = np.meshgrid(lon, lat)
+    mlon, mlat = np.meshgrid(lon, lat)
 
     plt.figure(4)
     plt.clf()
-    plt.scatter(x, y, c=tsurf - 273.15, marker="s")
+    plt.scatter(mlon, mlat, c=tsurf - 273.15, marker="s")
     plt.axis([-180, 180, -90, -60])
     plt.colorbar(label="Temperature ($^{o}$C)")
     plt.ylabel("Latitude")
@@ -39,26 +57,35 @@ def plot_final_results(lat, lon, frequency, depth, strength, tsurf):
     plt.figure(5)
     plt.clf()
     plt.subplot(311)
-    plt.scatter(x, y, c=frequency, marker="s")  # , cmap="jet")
+    plt.scatter(mlon, mlat, c=frequency, marker="s")
     plt.colorbar(label="Frequency (%)")
     plt.ylabel("Latitude")
     plt.axis([-180, 180, -90, -60])
 
     plt.subplot(312)
-    plt.scatter(x, y, c=depth * 1000, marker="s")  # , cmap="jet")
+    plt.scatter(mlon, mlat, c=depth * 1000, marker="s")
     plt.colorbar(label="Depth (m)")
     plt.ylabel("Latitude")
     plt.axis([-180, 180, -90, -60])
 
     plt.subplot(313)
-    plt.scatter(x, y, c=strength, marker="s")  # , cmap="jet")
+    plt.scatter(mlon, mlat, c=strength, marker="s")
     plt.colorbar(label="Intensity (K)")
     plt.axis([-180, 180, -90, -60])
     plt.ylabel("Latitude")
     plt.xlabel("Longitude")
 
 
-def plot_results(lat, lon, ncases, tsurfsum, ninversions, depthsum, intensum):
+def plot_results(
+    lat: npt.NDArray[np.float64],
+    lon: npt.NDArray[np.float64],
+    ncases: npt.NDArray[np.float64],
+    tsurfsum: npt.NDArray[np.float64],
+    ninversions: npt.NDArray[np.float64],
+    depthsum: npt.NDArray[np.float64],
+    intensum: npt.NDArray[np.float64],
+):
+
     """
     Plot results
     @params lat  The latitude
@@ -77,7 +104,11 @@ def plot_results(lat, lon, ncases, tsurfsum, ninversions, depthsum, intensum):
     plot_final_results(lat, lon, frequency, depth, strength, tsurf)
 
 
-def plot_for_year(year, ncfile):
+def plot_for_year(ncfile: str):
+    """
+    Plot results for a single year (by computing means from sums)
+    @params ncfile  The file containing the results for the year
+    """
     with Dataset(ncfile) as ncid:
         plot_results(
             ncid["lat"][:],
@@ -90,12 +121,14 @@ def plot_for_year(year, ncfile):
         )
 
 
-if __name__ == "__main__":
-    # Selected year
-    year = 2018
+# Example of use:
+#
+# if __name__ == "__main__":
+#     # Selected year
+#     year = 2018
 
-    # Directory and output file
-    ncdir = "era5/" + str(year) + "/"
-    ncfile = "era5/inversion_stats_" + str(year) + ".nc"
+#     # Directory and output file
+#     ncdir = "era5/" + str(year) + "/"
+#     ncfile = "era5/inversion_stats_" + str(year) + ".nc"
 
-    plot_for_year(year, ncfile)
+#     plot_for_year(ncfile)
